ec4c431202546f8cca8c2d337c0a71a5a390d749
babtem-eng <babtem@gmail.com>
2026-03-14 15:08:53 +0000

    Logo Update

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,12 +5,7 @@ export default function Header() {
     <header className="header">
       <div className="container header-content">
         <Link href="/" className="brand">
-          <svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2.5" strokeLinecap="round" strokeLinejoin="round">
-            <path d="M4 22h16a2 2 0 0 0 2-2V4a2 2 0 0 0-2-2H8a2 2 0 0 0-2 2v16a2 2 0 0 1-2 2Zm0 0a2 2 0 0 1-2-2v-9c0-1.1.9-2 2-2h2"/>
-            <path d="M18 14h-8"/>
-            <path d="M15 18h-5"/>
-            <path d="M10 6h8v4h-8V6Z"/>
-          </svg>
+          <img src="/logo.png" alt="Naija News Hub" style={{ width: '32px', height: '32px', borderRadius: '6px', objectFit: 'cover' }} />
           Naija News Hub
         </Link>
       </div>
